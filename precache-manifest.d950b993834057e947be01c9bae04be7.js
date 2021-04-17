13f4d0f58e9dee6ce2a156a833fd782ad6892789
Azul Alysum <azulalysum@Azuls-MacBook-Pro.local>
2021-04-18 05:49:06 +1200

    Updates

---
+++ precache-manifest.d950b993834057e947be01c9bae04be7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794deb66cb4dd5ed44a148a4d8874535",
+    "url": "/firearian/index.html"
+  },
+  {
+    "revision": "a69a9170d562de3d9268",
+    "url": "/firearian/static/css/main.9a50d586.chunk.css"
+  },
+  {
+    "revision": "c7d3bf0a602b340a4a38",
+    "url": "/firearian/static/js/2.af758700.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/firearian/static/js/2.af758700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a69a9170d562de3d9268",
+    "url": "/firearian/static/js/main.6129ee9e.chunk.js"
+  },
+  {
+    "revision": "1c3130ae933e405a1eeb",
+    "url": "/firearian/static/js/runtime-main.bf77e6fc.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/firearian/static/media/logo.495d93cc.svg"
+  }
+]);
